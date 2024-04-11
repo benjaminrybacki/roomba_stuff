949aa9a3a032a1a42db595fa8b3fcb17dfcd423e
benjaminrybacki <39493502+benjaminrybacki@users.noreply.github.com>
2024-04-11 11:33:51 -0500

    sending grid data to flask server with button

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def index():
   return render_template('index.html')
+
+# create a route to catch the grid that is posted
+@app.route('/calculate_path', methods=['POST'])
+def calculate_path():
+  # get the grid from the post request
+  json_grid = request.get_json()
+  print(json_grid)
+  # parse the grid out of the json
+  grid = json_grid['grid']
+  # TODO: show loading somehow  while we are waiting for the path to finish
+  # calculate the path
+  path = calculate_path(grid)
+  return render_template('index.html', path=path)
+
+# function to calculate path
+def calculate_path(grid):
+  path = []
+  # TODO: Chance algorithm here
+  return path
 
 if __name__ == '__main__':
   app.run()
--- static/script.js
+++ static/script.js
@@ -13,3 +13,59 @@ gridBoxes.forEach((box) => {
   });
 });
 
+// a function that sends a 2d array representation of the grid to the flask server
+function calculatePath() {
+  // TODO: show loading spinner
+  let grid = [];
+  let row = [];
+  let gridBoxes = document.querySelectorAll('.grid-box');
+  let index = 0;
+  gridBoxes.forEach((box) => {
+      if (box.classList.contains('roomba')) {
+        row.push(2);
+      } else if (box.classList.contains('occupied')) {
+        row.push(1);
+      } else {
+        row.push(0);
+      }
+      index++;
+      
+      if(index == 5) {
+        grid.push(row);
+        row = [];
+        index = 0;
+      }
+  });
+
+  fetch('/calculate_path', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ grid: grid })
+  })
+}
+
+// TODO: Fix the drag and drop functionality
+// Make sure roomba can be dropped anywhere on the grid
+// show roomba being dragged
+let roomba = document.getElementById('roomba');
+console.log(roomba)
+
+roomba.addEventListener('dragstart', (e) => {
+  e.preventDefault();
+  console.log('dragging');
+  roomba.classList.add('dragging');
+});
+
+roomba.addEventListener('drop', (e) => {
+  e.preventDefault();
+  console.log(e)
+  if (e.target.classList.contains('grid-box') 
+    && !e.target.classList.contains('roomba')
+    && !e.target.classList.contains('occupied')) {
+    roomba.classList.remove('roomba');
+    e.target.classList.add('roomba');
+    roomba = e.target;
+  }
+});
